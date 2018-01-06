f070701cee9faf5b5147f0ad86daa31027a85f29
BenHigginbottom <ben.higginbottom@gmail.com>
2018-01-06 13:53:58 +0000

    Added twitter bot

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ My AWS Lambda Scripts
 route53subDomains.py :  
 Allows for the creation of any number of subdomains under a master record by being called via the API Gateway into a Lambda backend
 Runs with 128Mb of memory and a 10 second timeout
+
+Twit_QEAircraft.py :
+Abandoned twitter bot that was started for UKDJ and TheOtherChris, when we got sick of the "Queen Elizabeth doesnt have any Aircraft" nonsense.
+
+Then I realised how much it would cost me to run...
+
+AWSTidy* :
+
+Housekeeping for AWS accounts
---
+++ Twit_QEAircraft.py
@@ -0,0 +1,47 @@
+import json
+import re
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+#Variables that contains the user credentials to access Twitter API 
+access_token = "ENTER YOUR ACCESS TOKEN"
+access_token_secret = "ENTER YOUR ACCESS TOKEN SECRET"
+consumer_key = "ENTER YOUR API KEY"
+consumer_secret = "ENTER YOUR API SECRET"
+
+
+def words_in_text(word, text):
+    word = word.lower()
+    text = text.lower()
+    match = re.search(word, text)
+    if match:
+        return True
+    return False
+
+
+#This is a basic listener that just prints received tweets to stdout.
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print data
+        return True
+
+    def on_error(self, status):
+        print status
+
+
+if __name__ == '__main__':
+
+    #Auth against the streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+
+    stream.filter(track=['Queen Elizabeth', 'no', 'aircraft'])
+
+    tweets['sentence'] = tweets['text'].apply(lambda tweet: words_in_text('Queen Elizabeth has no aircraft', tweet))
+
+
+
