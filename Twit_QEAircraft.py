251bf3507ef8d9c94795c3674e156c30aa694e5f
BenHigginbottom <ben.higginbottom@gmail.com>
2018-01-09 15:00:08 +0000

    Refactored to make things alot simpler

--- Twit_QEAircraft.py
+++ Twit_QEAircraft.py
@@ -1,47 +1,45 @@
-import json
-import re
-from tweepy.streaming import StreamListener
-from tweepy import OAuthHandler
-from tweepy import Stream
+#!/usr/bin/env python
+import tweepy
 
-#Variables that contains the user credentials to access Twitter API 
-access_token = "ENTER YOUR ACCESS TOKEN"
-access_token_secret = "ENTER YOUR ACCESS TOKEN SECRET"
-consumer_key = "ENTER YOUR API KEY"
-consumer_secret = "ENTER YOUR API SECRET"
+CONSUMER_KEY = "CONSUMER_KEY"
+CONSUMER_SECRET = "CONSUMER_SECRET"
+ACCESS_TOKEN = "ACCESS_TOKEN"
+ACCESS_TOKEN_SECRET = "ACCESS_TOKEN_SECRET"
 
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
 
-def words_in_text(word, text):
-    word = word.lower()
-    text = text.lower()
-    match = re.search(word, text)
-    if match:
-        return True
-    return False
+search = api.search(q="Queen Elizabeth")
 
+aircraft = ['aircraft carrier no aircraft',
+            'no aircraft',
+            'aircraft carrier without aircraft',
+            'aircraft carrier no planes',
+            'no planes',
+            'without planes']
 
-#This is a basic listener that just prints received tweets to stdout.
-class StdOutListener(StreamListener):
+jets = ['aircraft carrier no jets',
+        'no jets',
+        'without jets',
+        'aircraft carrier without jets']
 
-    def on_data(self, data):
-        print data
-        return True
+leaking = ['is sinking',
+           'is leaking']
 
-    def on_error(self, status):
-        print status
-
-
-if __name__ == '__main__':
-
-    #Auth against the streaming API
-    l = StdOutListener()
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    stream = Stream(auth, l)
-
-    stream.filter(track=['Queen Elizabeth', 'no', 'aircraft'])
-
-    tweets['sentence'] = tweets['text'].apply(lambda tweet: words_in_text('Queen Elizabeth has no aircraft', tweet))
-
-
-
+for s in search:
+    for i in aircraft:
+        if i == s.text:
+            screen_name = s.user.screen_name
+            response = "@%s Helicopters are aircraft" % screen_name
+            s = api.update_status(response, s.id)
+    for i in jets:
+        if i == s.text:
+            screen_name = s.user.screen_name
+            response = "@%s The F35 testing begins this year" % screen_name
+            s = api.update_status(response, s.id)
+    for i in leaking:
+        if i == s.text:
+            screen_name = s.user.screen_name
+            response = "@%s This is what sea trials are for, issues will be fixed!" % screen_name
+            s = api.update_status(response, s.id)
